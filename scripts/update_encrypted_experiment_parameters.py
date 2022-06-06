dbabc36bc170fa2d92e6b67b57b2c79881cfe954
Eric W <eawolf@mit.edu>
2022-06-06 11:56:21 -0400

    Added scripts for updating plain- and ciphertext experiment parameters; added binary file with encrypted experiment parameters.

---
+++ scripts/update_encrypted_experiment_parameters.py
@@ -0,0 +1,17 @@
+import sys 
+import os 
+
+path_to_file = os.path.dirname(os.path.abspath(__file__))
+path_to_analysis = path_to_file + "/../../"
+
+sys.path.insert(0, path_to_analysis)
+
+from BEC1_Analysis.code import crypto_functions 
+
+
+def main():
+    crypto_functions.update_encrypted_experiment_parameters() 
+
+
+if __name__ == "__main__":
+    main()
