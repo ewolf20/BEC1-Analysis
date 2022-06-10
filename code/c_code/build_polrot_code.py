625110f2c99b4f0d677891eca52e84d5e64fb6cf
Eric W <eawolf@mit.edu>
2022-06-10 13:21:36 -0400

    Initial writing and testing of c code for polarization rotation image calculation; works, but not interfaced with scipy or benchmarked yet

---
+++ code/c_code/build_polrot_code.py
@@ -0,0 +1,47 @@
+from cffi import FFI 
+
+
+
+source_string = r"""
+    #include <math.h>
+
+    static double imaging_od_lorentzian(double detuning, double linewidth, double intensity, double intensity_sat) {
+        return 1.0 / (1.0 + pow(2.0 * detuning / linewidth, 2.0) + pow(intensity / intensity_sat, 2.0));
+    }
+
+    static int give_polrot_image(float *od_vector, double detuning_1A, double detuning_1B, double detuning_2A, double detuning_2B, 
+                                double intensity_A, double intensity_B, double intensity_sat, double res_cross_section, double linewidth,
+                                double *buff_out) {
+        double od_naught_1 = od_vector[0];
+        double od_naught_2 = od_vector[1];
+        double od_1A = od_naught_1 * imaging_od_lorentzian(detuning_1A, linewidth, intensity_A, intensity_sat);
+        double od_1B = od_naught_1 * imaging_od_lorentzian(detuning_1B, linewidth, intensity_A, intensity_sat);
+        double od_2A = od_naught_2 * imaging_od_lorentzian(detuning_2A, linewidth, intensity_B, intensity_sat);
+        double od_2B = od_naught_2 * imaging_od_lorentzian(detuning_2B, linewidth, intensity_B, intensity_sat);
+        double phi_A = - detuning_1A / linewidth * od_1A - detuning_2A / linewidth * od_2A;
+        double phi_B = - detuning_1B / linewidth * od_1B - detuning_2B / linewidth * od_2B;
+        double abs_A = exp(-od_1A / 2.0) * exp(-od_2A / 2.0);
+        double abs_B = exp(-od_1B / 2.0) * exp(-od_2B / 2.0);
+        double result_A = 0.5 + pow(abs_A, 2.0) / 2 + abs_A * sin(phi_A);
+        double result_B = 0.5 + pow(abs_B, 2.0) / 2 + abs_B * sin(phi_B);
+        buff_out[0] = result_A;
+        buff_out[1] = result_B;
+        return 0;
+    }
+"""
+
+ffibuilder = FFI()
+ffibuilder.cdef("""int give_polrot_image(float *, double detuning_1A, double detuning_1B, double detuning_2A, double detuning_2B, 
+                                        double intensity_A, double intensity_B, double intensity_sat, double res_cross_section, double linewidth,
+                                         double *);""")
+
+ffibuilder.set_source("_polrot_code", source_string)
+
+
+if __name__ == "__main__":
+    ffibuilder.compile(verbose = True) 
+
+
+"""
+
+        """
---
+++ code/c_code/test_polrot_code.py
@@ -0,0 +1,31 @@
+import ctypes
+
+import numpy as np 
+
+from _polrot_code import ffi, lib 
+
+
+def main():
+    detuning_1A = 1.0
+    detuning_1B = 0.0
+    detuning_2A = 1.0 
+    detuning_2B = 4.0 
+    intensity_A = 0.0
+    intensity_B = 0.0 
+    linewidth = 100.0
+    res_cross_section = 1.0
+    intensity_sat = 1000.0
+    double_buffer_out = ffi.new("double[]", 2)
+    od_vector = ffi.new("float[]", [1.0, 2.0])
+    result = lib.give_polrot_image(od_vector, detuning_1A, detuning_1B, detuning_2A, 
+                                    detuning_2B, intensity_A, intensity_B, intensity_sat,
+                                    res_cross_section, linewidth, double_buffer_out)
+    print(result) 
+    print(double_buffer_out[0]) 
+    print(double_buffer_out[1])
+
+
+
+
+if __name__ == "__main__":
+    main()
