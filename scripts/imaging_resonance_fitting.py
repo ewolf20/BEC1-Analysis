73752508064de4f27477e452d01cca454692aaf5
Eric W <eawolf@mit.edu>
2022-07-01 17:20:01 -0400

    Added some initial science functions, as weel as scripts for imaging resonance fitting

---
+++ scripts/imaging_resonance_fitting.py
@@ -0,0 +1,59 @@
+import os 
+import sys 
+
+import numpy as np 
+import matplotlib.pyplot as plt
+
+PATH_TO_GRADSCHOOL_FOLDER = "/Users/eric/Codevault/GradSchool"
+
+
+sys.path.insert(0, PATH_TO_GRADSCHOOL_FOLDER) 
+
+from BEC1_Analysis.code import image_processing_functions, data_fitting_functions
+from imaging_resonance_processing import get_workfolder_path
+
+
+UPPER_COUNTS_CUTOFF = 1e5
+LOWER_COUNTS_CUTOFF = 1e3
+
+def main():
+    file_path = get_data_file_input() 
+    frequencies, counts = np.load(file_path) 
+    frequencies_trimmed, counts_trimmed = zip(*[f for f in zip(frequencies, counts) if f[1] < UPPER_COUNTS_CUTOFF and f[1] > LOWER_COUNTS_CUTOFF])
+    frequencies_trimmed = list(frequencies_trimmed)
+    counts_trimmed = list(counts_trimmed) 
+    fit_results = data_fitting_functions.fit_imaging_resonance_lorentzian(frequencies_trimmed, counts_trimmed) 
+    popt, pcov = fit_results
+    fit_report = data_fitting_functions.fit_report(data_fitting_functions.imaging_resonance_lorentzian, fit_results)
+    fit_values = data_fitting_functions.imaging_resonance_lorentzian(frequencies_trimmed, *popt)
+    frequencies_sorted, fit_values_sorted = zip(*sorted(zip(frequencies_trimmed, fit_values), key = lambda f: f[0]))
+    title = os.path.basename(file_path).split('.')[0]
+    with open(os.path.join(get_workfolder_path(), title + "_Fit_Report.txt"), 'w') as f:
+        f.write(fit_report) 
+    print(fit_report)
+    plt.plot(frequencies_trimmed, counts_trimmed, 'o', label = "Data") 
+    plt.plot(frequencies_sorted, fit_values_sorted, label = "Fit to data") 
+    plt.xlabel("Nominal Freq (MHz)") 
+    plt.ylabel("Counts (arb)")
+    plt.legend() 
+    plt.suptitle(title)
+    plt.savefig(os.path.join(get_workfolder_path(), title + "_Graph.png"))
+    plt.show()
+    
+
+
+
+
+def get_data_file_input():
+    print("Please enter the name of or full absolute path to the .npy file containing the processed counts data.") 
+    print("If path is not specified, the file will be searched for in today's directory in Private_BEC1_Analysis.")
+    user_input = input()
+    if not os.path.isfile(user_input):
+        file_path = os.path.join(get_workfolder_path(), user_input)
+    else:
+        file_path = user_input
+    return file_path 
+
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/imaging_resonance_processing.py
@@ -0,0 +1,111 @@
+import datetime
+import os 
+import sys 
+
+import numpy as np 
+import matplotlib.pyplot as plt
+
+path_to_file = os.path.dirname(os.path.abspath(__file__))
+path_to_repo_folder = os.path.abspath(path_to_file + "/../../")
+
+
+sys.path.insert(0, path_to_repo_folder)
+
+from satyendra.code.image_watchdog import ImageWatchdog
+from BEC1_Analysis.code.measurement import Run, Measurement
+from BEC1_Analysis.code import image_processing_functions, data_fitting_functions
+
+SUPPORTED_IMAGE_TYPES = ['side_lf', 'side_hf', 'topA', 'topB']
+
+
+ROI_COORDINATES = None
+NORM_BOX_COORDINATES = None 
+
+def main():
+    measurement_directory_path = get_measurement_directory_input()
+    imaging_mode_string = get_imaging_mode()
+    run_image_name, frequency_key, imaging_type_key = imaging_mode_decoder(imaging_mode_string)
+    workfolder_pathname = get_workfolder_path()
+    if(not os.path.isdir(workfolder_pathname)):
+        os.makedirs(workfolder_pathname)
+    ImageWatchdog.clean_filenames(measurement_directory_path, image_type_default = run_image_name)
+    my_measurement = Measurement(measurement_directory_path, hold_images_in_memory = False, run_parameters_verbose = True, imaging_type = imaging_type_key)
+    run_params_filename = os.path.join(workfolder_pathname, imaging_mode_string + "_runs_dump.json")
+    use_saved_params = os.path.isfile(run_params_filename)
+    print("Initializing")
+    my_measurement._initialize_runs_dict(use_saved_params = use_saved_params, saved_params_filename = run_params_filename)
+    if(not use_saved_params):
+        my_measurement.dump_runs_dict(dump_filename = run_params_filename)
+    my_measurement.set_box("ROI", box_coordinates = ROI_COORDINATES)
+    my_measurement.set_norm_box(box_coordinates = NORM_BOX_COORDINATES)
+    frequencies_list = [] 
+    counts_list = []
+    for run_id in my_measurement.runs_dict:
+        print(str(run_id))
+        current_run = my_measurement.runs_dict[run_id] 
+        current_image_stack = current_run.get_image(run_image_name, memmap = True)
+        current_od_image = image_processing_functions.get_absorption_od_image(current_image_stack, ROI = my_measurement.measurement_parameters['ROI'])
+        current_counts = image_processing_functions.pixel_sum(current_od_image)
+        counts_list.append(current_counts)
+        frequencies_list.append(current_run.parameters[frequency_key])
+    frequencies_array = np.array(frequencies_list)
+    counts_array = np.array(counts_list)
+    data_saving_path = os.path.join(workfolder_pathname, "Data_" + imaging_mode_string + ".npy")
+    np.save(data_saving_path, np.stack((frequencies_array, counts_array)))
+    plt.plot(frequencies_array, counts_array, 'o') 
+    plt.show()
+
+
+
+def get_measurement_directory_input():
+    print("Please enter the path to the measurement directory containing the runs to analyze.")
+    user_input = input()
+    if(not os.path.isdir(user_input)):
+        raise RuntimeError("Unable to find the measurement directory.")
+    return user_input
+
+
+def get_imaging_mode():
+    print("Please enter the imaging mode. Supported options are: ") 
+    for image_type in SUPPORTED_IMAGE_TYPES:
+        print(image_type)
+    user_input = input() 
+    if(not user_input in SUPPORTED_IMAGE_TYPES):
+        raise RuntimeError("Specified imaging type is not supported.")
+    return user_input
+
+
+def imaging_mode_decoder(imaging_mode_string):
+    SIDE_RUN_IMAGE_NAME = "Side" 
+    TOP_A_RUN_IMAGE_NAME = "TopA"
+    TOP_B_RUN_IMAGE_NAME = "TopB" 
+    SIDE_LF_FREQ_KEY = "LFImgFreq"
+    SIDE_HF_FREQ_KEY = "ImagFreq0"
+    TOP_A_FREQ_KEY = "ImagFreq1"
+    TOP_B_FREQ_KEY = "ImagFreq2" 
+    SIDE_LF_MEASUREMENT_KEY = "side_low_mag"
+    SIDE_HF_MEASUREMENT_KEY = "side_high_mag"
+    TOP_MEASUREMENT_KEY = "top_double"
+    if(imaging_mode_string == "side_lf"):
+        return (SIDE_RUN_IMAGE_NAME, SIDE_LF_FREQ_KEY, SIDE_LF_MEASUREMENT_KEY)
+    elif(imaging_mode_string == "side_hf"):
+        return (SIDE_RUN_IMAGE_NAME, SIDE_HF_FREQ_KEY, SIDE_HF_MEASUREMENT_KEY)
+    elif(imaging_mode_string == "topA"):
+        return (TOP_A_RUN_IMAGE_NAME, TOP_A_FREQ_KEY, TOP_MEASUREMENT_KEY)
+    elif(imaging_mode_string == "topB"):
+        return (TOP_B_RUN_IMAGE_NAME, TOP_B_FREQ_KEY, TOP_MEASUREMENT_KEY)
+
+
+def get_workfolder_path():
+    PRIVATE_DIRECTORY_REPO_NAME = "Private_BEC1_Analysis"
+    path_to_private_directory_repo = os.path.join(path_to_repo_folder, PRIVATE_DIRECTORY_REPO_NAME)
+    current_datetime = datetime.datetime.now() 
+    current_year = current_datetime.strftime("%Y")
+    current_year_month = current_datetime.strftime("%Y-%m")
+    current_year_month_day = current_datetime.strftime("%Y-%m-%d")
+    workfolder_pathname = os.path.join(path_to_private_directory_repo, current_year, current_year_month, current_year_month_day + "_Imaging_Resonance")
+    return workfolder_pathname
+
+
+if __name__ == "__main__":
+    main()
