4bc20338acaab07e8f726bd8e5add7464af76ab6
Eric W <eawolf@mit.edu>
2022-09-20 18:18:12 -0400

    Update imaging_resonance_processing to support TopAB flag & use saved run params intelligently

--- scripts/imaging_resonance_processing.py
+++ scripts/imaging_resonance_processing.py
@@ -15,7 +15,7 @@ from satyendra.code.image_watchdog import ImageWatchdog
 from BEC1_Analysis.code.measurement import Run, Measurement
 from BEC1_Analysis.code import image_processing_functions, data_fitting_functions
 
-SUPPORTED_IMAGE_TYPES = ['side_lf', 'side_hf', 'topA', 'topB']
+SUPPORTED_IMAGE_TYPES = ['Side_lf', 'Side_hf', 'TopA', 'TopB', 'TopAB']
 
 
 ROI_COORDINATES = None
@@ -24,50 +24,58 @@ NORM_BOX_COORDINATES = None
 def main():
     measurement_directory_path = get_measurement_directory_input()
     imaging_mode_string = get_imaging_mode()
-    run_image_name, frequency_key, imaging_type_key = imaging_mode_decoder(imaging_mode_string)
     workfolder_pathname = get_workfolder_path()
     if(not os.path.isdir(workfolder_pathname)):
         os.makedirs(workfolder_pathname)
-    ImageWatchdog.clean_filenames(measurement_directory_path, image_type_default = run_image_name)
+    run_image_name_list, frequency_key_list, imaging_type_key = imaging_mode_decoder(imaging_mode_string)
     my_measurement = Measurement(measurement_directory_path, hold_images_in_memory = False, run_parameters_verbose = True, imaging_type = imaging_type_key)
-    run_params_filename = os.path.join(workfolder_pathname, imaging_mode_string + "_runs_dump.json")
-    use_saved_params = os.path.isfile(run_params_filename)
     print("Initializing")
-    my_measurement._initialize_runs_dict(use_saved_params = use_saved_params, saved_params_filename = run_params_filename)
-    if(not use_saved_params):
-        my_measurement.dump_runs_dict(dump_filename = run_params_filename)
+    saved_params_filename = os.path.join(workfolder_pathname, imaging_type_key + "_runs_dump.json")
+    if(os.path.isfile(saved_params_filename)):
+        try:
+            my_measurement._initialize_runs_dict(use_saved_params = True)
+        except RuntimeError:
+            my_measurement._initialize_runs_dict(use_saved_params = False)
+            my_measurement.dump_runs_dict(dump_filename = saved_params_filename)
+    else:
+        my_measurement._initialize_runs_dict(use_saved_params = False) 
+        my_measurement.dump_runs_dict(dump_filename = saved_params_filename)
     run_to_use = 0
     box_set = False
     while (not box_set) and run_to_use < len(my_measurement.runs_dict):
         try:
-            my_measurement.set_ROI(box_coordinates = ROI_COORDINATES, run_to_use = run_to_use, image_to_use = run_image_name)
-            my_measurement.set_norm_box(box_coordinates = NORM_BOX_COORDINATES, run_to_use = run_to_use, image_to_use = run_image_name)
+            my_measurement.set_ROI(box_coordinates = ROI_COORDINATES, run_to_use = run_to_use, image_to_use = run_image_name_list[0])
+            my_measurement.set_norm_box(box_coordinates = NORM_BOX_COORDINATES, run_to_use = run_to_use, image_to_use = run_image_name_list[0])
         except TypeError:
             pass 
         else:
             box_set = True
         run_to_use += 1
-    frequencies_list = []
-    counts_list = []
-    for run_id in my_measurement.runs_dict:
-        print(str(run_id))
-        current_run = my_measurement.runs_dict[run_id]
-        current_image_stack = current_run.get_image(run_image_name, memmap = True)
-        current_od_image = image_processing_functions.get_absorption_od_image(current_image_stack, ROI = my_measurement.measurement_parameters['ROI'])
-        current_counts = image_processing_functions.pixel_sum(current_od_image)
-        counts_list.append(current_counts)
-        nominal_frequency = current_run.parameters[frequency_key] 
-        if(imaging_mode_string == "side_lf"):
-            frequency_multiplier = my_measurement.experiment_parameters["li_lf_freq_multiplier"]
+    for run_image_name, frequency_key in zip(run_image_name_list, frequency_key_list):
+        frequencies_list = []
+        counts_list = []
+        for run_id in my_measurement.runs_dict:
+            print(str(run_id))
+            current_run = my_measurement.runs_dict[run_id]
+            current_image_stack = current_run.get_image(run_image_name, memmap = True)
+            current_od_image = image_processing_functions.get_absorption_od_image(current_image_stack, ROI = my_measurement.measurement_parameters['ROI'])
+            current_counts = image_processing_functions.pixel_sum(current_od_image)
+            counts_list.append(current_counts)
+            nominal_frequency = current_run.parameters[frequency_key] 
+            if(imaging_mode_string == "side_lf"):
+                frequency_multiplier = my_measurement.experiment_parameters["li_lf_freq_multiplier"]
+            else:
+                frequency_multiplier = my_measurement.experiment_parameters["li_hf_freq_multiplier"]
+            frequencies_list.append(nominal_frequency * frequency_multiplier)
+        frequencies_array = np.array(frequencies_list)
+        counts_array = np.array(counts_list)
+        if(not imaging_mode_string == "TopAB"):
+            data_saving_path = os.path.join(workfolder_pathname, "Data_" + imaging_mode_string + ".npy")
         else:
-            frequency_multiplier = my_measurement.experiment_parameters["li_hf_freq_multiplier"]
-        frequencies_list.append(nominal_frequency * frequency_multiplier)
-    frequencies_array = np.array(frequencies_list)
-    counts_array = np.array(counts_list)
-    data_saving_path = os.path.join(workfolder_pathname, "Data_" + imaging_mode_string + ".npy")
-    np.save(data_saving_path, np.stack((frequencies_array, counts_array)))
-    plt.plot(frequencies_array, counts_array, 'o') 
-    plt.show()
+            data_saving_path = os.path.join(workfolder_pathname, "Data_" + run_image_name + ".npy")
+        np.save(data_saving_path, np.stack((frequencies_array, counts_array)))
+        plt.plot(frequencies_array, counts_array, 'o')
+        plt.show()
 
 
 
@@ -100,14 +108,16 @@ def imaging_mode_decoder(imaging_mode_string):
     SIDE_LF_MEASUREMENT_KEY = "side_low_mag"
     SIDE_HF_MEASUREMENT_KEY = "side_high_mag"
     TOP_MEASUREMENT_KEY = "top_double"
-    if(imaging_mode_string == "side_lf"):
-        return (SIDE_RUN_IMAGE_NAME, SIDE_LF_FREQ_KEY, SIDE_LF_MEASUREMENT_KEY)
-    elif(imaging_mode_string == "side_hf"):
-        return (SIDE_RUN_IMAGE_NAME, SIDE_HF_FREQ_KEY, SIDE_HF_MEASUREMENT_KEY)
-    elif(imaging_mode_string == "topA"):
-        return (TOP_A_RUN_IMAGE_NAME, TOP_A_FREQ_KEY, TOP_MEASUREMENT_KEY)
-    elif(imaging_mode_string == "topB"):
-        return (TOP_B_RUN_IMAGE_NAME, TOP_B_FREQ_KEY, TOP_MEASUREMENT_KEY)
+    if(imaging_mode_string == "Side_lf"):
+        return ([SIDE_RUN_IMAGE_NAME], [SIDE_LF_FREQ_KEY], SIDE_LF_MEASUREMENT_KEY)
+    elif(imaging_mode_string == "Side_hf"):
+        return ([SIDE_RUN_IMAGE_NAME], [SIDE_HF_FREQ_KEY], SIDE_HF_MEASUREMENT_KEY)
+    elif(imaging_mode_string == "TopA"):
+        return ([TOP_A_RUN_IMAGE_NAME], [TOP_A_FREQ_KEY], TOP_MEASUREMENT_KEY)
+    elif(imaging_mode_string == "TopB"):
+        return ([TOP_B_RUN_IMAGE_NAME], [TOP_B_FREQ_KEY], TOP_MEASUREMENT_KEY)
+    elif(imaging_mode_string == "TopAB"):
+        return ([TOP_A_RUN_IMAGE_NAME, TOP_B_RUN_IMAGE_NAME], [TOP_A_FREQ_KEY, TOP_B_FREQ_KEY], TOP_MEASUREMENT_KEY)
 
 
 def get_workfolder_path():
