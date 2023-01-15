276af3b64d15489ee27a06965343d7c2662e3722
Eric W <eawolf@mit.edu>
2023-01-15 10:09:41 -0500

    Add box shake-related functions to analysis functions

--- code/analysis_functions.py
+++ code/analysis_functions.py
@@ -1,7 +1,7 @@
 import numpy as np
 from scipy.integrate import trapezoid 
 
-from . import image_processing_functions, science_functions
+from . import data_fitting_functions, image_processing_functions, science_functions
 
 
 #PIXEL SUMS
@@ -149,6 +149,11 @@ def get_atom_count_top_B_abs(my_measurement, my_run, state_index = 3, stored_den
     pixel_area = np.square(my_measurement.experiment_parameters["top_um_per_pixel"])
     return image_processing_functions.atom_count_pixel_sum(atom_density, pixel_area)
 
+def get_atom_counts_top_AB_abs(my_measurement, my_run, first_state_index = 1, second_state_index = 3, 
+                                first_stored_density_name = None, second_stored_density_name = None):
+    return (get_atom_count_top_A_abs(my_measurement, my_run, state_index = first_state_index, stored_density_name=first_stored_density_name), 
+            get_atom_count_top_B_abs(my_measurement, my_run, state_index = second_state_index, stored_density_name=second_stored_density_name))
+
 def get_atom_counts_top_polrot(my_measurement, my_run, first_state_index = 1, second_state_index = 3, first_stored_density_name = None, 
                                 second_stored_density_name = None):
     atom_density_first, atom_density_second = _load_densities_polrot(my_measurement, my_run, first_state_index, second_state_index, 
@@ -160,7 +165,7 @@ def get_atom_counts_top_polrot(my_measurement, my_run, first_state_index = 1, se
 
 #TRAP-SPECIFIC_ANALYSES
 
-#HYBRID TRAP
+#HYBRID TRAP - BOX EXP
 
 def get_hybrid_trap_densities_along_harmonic_axis_polrot(my_measurement, my_run, first_state_index = 1, second_state_index = 3, 
                                                     autocut = True,
@@ -188,7 +193,7 @@ def get_hybrid_trap_densities_along_harmonic_axis_polrot(my_measurement, my_run,
 def get_hybrid_trap_average_energy(my_measurement, my_run, first_state_index = 1, second_state_index = 3, 
                                     autocut = True,
                                     first_stored_density_name = None, second_stored_density_name = None):
-    positions_first, densities_first, positions_second, densities_second = get_hybrid_trap_densities_polrot_along_harmonic_axis( 
+    positions_first, densities_first, positions_second, densities_second = get_hybrid_trap_densities_along_harmonic_axis_polrot( 
                                                                     my_measurement, my_run, first_state_index = first_state_index, 
                                                                     second_state_index = second_state_index, autocut = autocut, 
                                                                     first_stored_density_name = first_stored_density_name, 
@@ -210,20 +215,75 @@ def get_hybrid_trap_average_energy(my_measurement, my_run, first_state_index = 1
 
 #BOX TRAP
 
-def get_box_shake_fourier_component(my_measurement, my_run, first_state_index = 1, second_state_index = 3, 
-                                        order = 1, no_shake_density_name = None, 
+"""
+Get the fourier component of designated order from box shake data. 
+
+Given a measurement and run, extract (1d) fourier components from the atom densities, integrated along the 
+non-shaken direction of the box. 
+
+Params:
+
+Most of the standard ones, plus, 
+
+order: The order of the fourier component to extract. If not passed, the dominant (nonzero) fourier amplitude will be 
+returned - though be warned that this may differ between runs!!!
+
+no_shake_density_name_(first, second): If not None, the function will assume that the atom density of the 
+(first/second) state with no box shaking is stored in measurement_analysis_results under the given name. 
+If None, the analysis will run without background subtraction."""
+def get_box_shake_fourier_amplitudes_polrot(my_measurement, my_run, first_state_index = 1, second_state_index = 3, 
+                                        order = None, no_shake_density_name_first = None, 
+                                        no_shake_density_name_second = None,
                                         first_stored_density_name = None, second_stored_density_name = None):
-    if no_shake_density_name is None:
-        no_shake_density = 0.0 
+    if no_shake_density_name_first is None:
+        no_shake_density_first = 0.0
     else:
-        no_shake_density = my_measurement.measurement_parameters[no_shake_density_name]
-    pass
+        no_shake_density_first = my_measurement.measurement_parameters[no_shake_density_name_first]
+    if no_shake_density_name_second is None:
+        no_shake_density_second = 0.0 
+    else:
+        no_shake_density_second = my_measurement.measurement_parameters[no_shake_density_name_second] 
+    atom_density_first, atom_density_second = _load_densities_polrot(my_measurement, my_run, first_state_index, 
+                                                second_state_index, first_stored_density_name, second_stored_density_name)
+    bs_density_first = atom_density_first - no_shake_density_first 
+    bs_density_second = atom_density_second - no_shake_density_second
+    #Current convention has the integration direction as the last index, i.e. the x-axis. 
+    integrated_density_first = np.sum(bs_density_first, axis = -1)
+    integrated_density_second = np.sum(bs_density_second, axis = -1)
+    x_delta = my_measurement.experiment_parameters["top_um_per_pixel"]
+    fft_results_first = data_fitting_functions.get_fft_peak(x_delta, integrated_density_first, order = order)
+    frequency_first, amp_first, phase_first = fft_results_first 
+    fft_results_second = data_fitting_functions.get_fft_peak(x_delta, integrated_density_second, order = order)
+    frequency_second, amp_second, phase_second = fft_results_second 
+    return (amp_first, amp_second)
 
 
-#ENTIRE-MEASUREMENT FUNCTIONS
+#MEASUREMENT-WIDE FUNCTIONS
 """
 Certain analyses must be run on an entire measurement, and are sufficiently common as to warrant 
-inclusion here - for instance, establishing a no_shake background for box shots. """
+inclusion here - for instance, establishing a no_shake background for box shots. These analyses have a different 
+calling signature, being called on fun(my_measurement, **kwargs)
+
+NOTE: It is _not_ appropriate to include functions here which involve only taking an average over run analyses; this 
+is better done by performing the analysis on all runs, then averaging over the results returned by get_analysis_value_from_runs."""
+
+
+def get_no_shake_average_profiles_polrot(my_measurement, first_state_index = 1, second_state_index = 3, 
+                                        first_stored_density_name = None, second_stored_density_name = None):
+    no_shake_sum_first = 0.0 
+    no_shake_sum_second = 0.0 
+    counter = 0 
+    for run_id in my_measurement.runs_dict:
+        current_run = my_measurement.runs_dict[run_id] 
+        if not current_run.is_badshot and current_run.parameters["ShakingCycles"] == 0:
+            density_first, density_second = _load_densities_polrot(my_measurement, current_run, first_state_index, 
+                                                    second_state_index, first_stored_density_name, second_stored_density_name)
+            no_shake_sum_first += density_first 
+            no_shake_sum_second += density_second
+            counter += 1 
+    no_shake_average_first = no_shake_sum_first / counter 
+    no_shake_average_second = no_shake_sum_second / counter
+    return (no_shake_average_first, no_shake_average_second)
 
 #UTILITY, NOT INTENDED FOR EXTERNAL CALLING
 def _get_resonance_frequency_from_state_index(my_measurement, state_index):
