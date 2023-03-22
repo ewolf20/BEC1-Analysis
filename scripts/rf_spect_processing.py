deeca9806710f368af87cc47729bce9ac969e1cb
Eric W <31462466+ewolf20@users.noreply.github.com>
2023-03-22 12:55:50 -0400

    Fix variable naming in rf_spect_processing script.

--- scripts/rf_spect_processing.py
+++ scripts/rf_spect_processing.py
@@ -129,7 +129,15 @@ def get_rf_frequencies_and_counts(my_measurement, resonance_key):
     state_index_A, state_index_B = get_state_indices_from_resonance_key(resonance_key)
     my_measurement.analyze_runs(analysis_functions.get_atom_counts_top_AB_abs, ("counts_A", "counts_B"), fun_kwargs = {"first_state_index":state_index_A, 
                                     "second_state_index":state_index_B}, print_progress = True)
-    rf_frequencies, counts_A = my_measurement.get_parameter_analysis_value_pair_from_runs("RF_Box_Center", "counts_A")
+    if(state_index_A in [2, 3] and state_index_B in [2, 3]):
+        rf_center_param_name = "RF23_Box_Center"
+    elif(state_index_A in [1, 2] and state_index_B in [1, 2]):
+        rf_center_param_name = "RF12_Box_Center"
+    elif(state_index_A in [1, 3] and state_index_B in [1, 3]):
+        rf_center_param_name = "RF13_Box_Center"
+    else:
+        raise ValueError("The combination of state indices isn't supported.")
+    rf_frequencies, counts_A = my_measurement.get_parameter_analysis_value_pair_from_runs(rf_center_param_name, "counts_A")
     counts_B = my_measurement.get_analysis_value_from_runs("counts_B")
     return (rf_frequencies, counts_A, counts_B, tau_value)
 
